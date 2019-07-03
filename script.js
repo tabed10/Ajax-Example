2a9a76ed99ee6812f87c9a992d3f8ea8e22d3569
cis101 <cis101@secs.iub.edu.bd>
2019-07-03 09:04:58 +0600

    cw

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 /* Add your script here */
-var addBtn = document.getElementById('add')
-addBtn.addEventListener('click', function(event){
-    var task = document.getElementById('Quantity')
-    var paragraph.innerText = '0'
-    task.appendChild(paragraph)
-})
-
